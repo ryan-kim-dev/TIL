338d284979c9408545a24be55cca8f806798187d
ryan-kim-dev <ryankim.h.dev@gmail.com>
2021-03-29 08:01:30 +0900

    node.js pratice

---
+++ nodejs/syntax/template.js
@@ -0,0 +1,11 @@
+var name = 'K8805';
+var letter = 'Dear '+name+'\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Kim aliqua. Ut enim ad minim veniam, quis nostrud '+name+' exercitation ullamco laboris nisi ut aliquip ex ea Kim commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse Kim cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, Kim sunt in culpa qui officia deserunt mollit anim id est laborum. '+name;
+console.log(letter);
+
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet,
+consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Kim aliqua. Ut enim ad minim veniam, quis nostrud ${name} exercitation ullamco laboris nisi ut aliquip ex ea Kim commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse Kim cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, ${5+5}
+Kim sunt in culpa qui officia deserunt
+ mollit anim id est laborum. ${name}`;
+console.log(letter);
